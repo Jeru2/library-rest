1c93c37aaf8e140ee0437dbdaab238e91fd3ed71
Madhur Shukla <madhur0312@gmail.com>
2021-07-29 20:01:32 +0530

    Updated request object to allow displaying requested endpoint

--- src/main/java/com/mindtree/libraryspring2/exception/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/mindtree/libraryspring2/exception/ExceptionHandlerControllerAdvice.java
@@ -3,11 +3,12 @@ package com.mindtree.libraryspring2.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -22,12 +23,13 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
 	Map<String, Object> response;
 	
 	@ExceptionHandler(value = {LibrarySpringException.class})
-	protected ResponseEntity<Map<String, Object>> handleConflict(LibrarySpringException ex, WebRequest request) 
+	protected ResponseEntity<Map<String, Object>> handleConflict(LibrarySpringException ex, HttpServletRequest request) 
 	{
 		response = new HashMap<String, Object>();
 		response.put("message", ex.getMessage());
 		response.put("HTTP Status", HttpStatus.BAD_REQUEST);	//The response code is sent here
 		response.put("code", "400");
+		response.put("requested URI", request.getRequestURI());
 		System.out.println("Handled an exception");
 		ex.printStackTrace();
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);	//The response code is sent here
@@ -39,12 +41,13 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
 	 * @return - ResponseEntity which contains response for user and status code
 	 */
 	@ExceptionHandler(value = {AuthorNotFoundException.class})
-	protected ResponseEntity<Map<String, Object>> handleConflict(AuthorNotFoundException ex, WebRequest request) 
+	protected ResponseEntity<Map<String, Object>> handleConflict(AuthorNotFoundException ex, HttpServletRequest request) 
 	{
 		response = new HashMap<String, Object>();
 		response.put("message", ex.getMessage());
 		response.put("HTTP Status", HttpStatus.NOT_FOUND);	//The response code is sent here
 		response.put("code", "404");
+		response.put("requested URI", request.getRequestURI());
 		System.out.println("Author not found exception");
 		ex.printStackTrace();
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);	
@@ -55,12 +58,13 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
 	 * @return - ResponseEntity which contains response for user and status code
 	 */
 	@ExceptionHandler(value = {BookNotFoundException.class})
-	protected ResponseEntity<Map<String, Object>> handleConflict(BookNotFoundException ex, WebRequest request) 
+	protected ResponseEntity<Map<String, Object>> handleConflict(BookNotFoundException ex, HttpServletRequest request) 
 	{
 		response = new HashMap<String, Object>();
 		response.put("message", ex.getMessage());
 		response.put("HTTP Status", HttpStatus.NOT_FOUND);	//The response code is sent here
 		response.put("code", "404");
+		response.put("requested URI", request.getRequestURI());
 		System.out.println("Book not found exception");
 		ex.printStackTrace();
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);	
@@ -71,12 +75,13 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
 	 * @return - ResponseEntity which contains response for user and status code
 	 */
 	@ExceptionHandler(value = {DuplicateInsertionException.class})
-	protected ResponseEntity<Map<String, Object>> handleConflict(DuplicateInsertionException ex, WebRequest request) 
+	protected ResponseEntity<Map<String, Object>> handleConflict(DuplicateInsertionException ex, HttpServletRequest request) 
 	{
 		response = new HashMap<String, Object>();
 		response.put("message", ex.getMessage());
 		response.put("HTTP Status", HttpStatus.CONFLICT);	//The response code is sent here
 		response.put("code", "409");
+		response.put("requested URI", request.getRequestURI());
 		System.out.println("Duplicate entry attempted exception");
 		ex.printStackTrace();
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);	
